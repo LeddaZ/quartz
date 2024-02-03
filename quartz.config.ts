4ec1283fdb1c95d1e2940f1637a6efa9905efcec
Leonardo Ledda <leonardoledda@gmail.com>
2024-02-03 19:22:51 +0100

    notes: Update base URL
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "notes-e2k.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
