d52df8cfc7106ae60b923f1c73be1b8f3746252c
Leonardo Ledda <leonardoledda@gmail.com>
2024-02-04 02:49:32 +0100

    Quartz sync: Feb 4, 2024, 2:49 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,9 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      "GitHub (my fork)": "https://github.com/LeddaZ/notes",
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/LeddaZ/notes",
+      "My website": "https://leddaz.github.io",
     },
   }),
 }
