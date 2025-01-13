fe2db09aca5b1c5d33cc68eccf5793177b0037ae
Leonardo Ledda <leonardoledda@gmail.com>
2025-01-13 09:41:48 +0100

    footer: Open links in new tab
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,7 +1,7 @@
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
-import style from "./styles/footer.scss"
 import { version } from "../../package.json"
 import { i18n } from "../i18n"
+import style from "./styles/footer.scss"
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 interface Options {
   links: Record<string, string>
@@ -15,12 +15,17 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a href="https://quartz.jzhao.xyz/" target="_blank">
+            Quartz v{version}
+          </a>{" "}
+          © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
-              <a href={link}>{text}</a>
+              <a href={link} target="_blank">
+                {text}
+              </a>
             </li>
           ))}
         </ul>
